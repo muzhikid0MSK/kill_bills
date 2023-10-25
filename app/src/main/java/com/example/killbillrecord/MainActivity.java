d45ce05b59e5c37f1a1b5e634a8ce2bbea3b1459
meishengke <631138727@qq.com>
2023-10-25 13:17:00 +0800

    - impl the on going notification

--- app/src/main/java/com/example/killbillrecord/MainActivity.java
+++ app/src/main/java/com/example/killbillrecord/MainActivity.java
@@ -2,20 +2,56 @@ package com.example.killbillrecord;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationChannel notificationChannel;
+    private NotificationManager notificationManager;
+
+    private Notification aliveNotification;
+
+    private static final String CHANNEL_ID = "ALIVE CHANNEL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        initView();
+        initHandler();
+
     }
 
-    private static final String CHANNEL_ID = "ALIVE CHANNEL";
+    private void initHandler() {
+
+    }
+
+    private void initData() {
+        createNotificationChannel();
+
+    }
+
+    private void initView() {
+        createAliveNotification();
+        int aliveNotiId = 1;
+        notificationManager.notify(aliveNotiId,aliveNotification);
+    }
+
+    private void createAliveNotification() {
+        aliveNotification = new Notification.Builder(MainActivity.this, CHANNEL_ID).
+                setContentTitle(getString(R.string.alive_notification_title)).
+                setContentText(getString(R.string.alive_notification_content)).
+                setSmallIcon(R.drawable.ic_launcher_foreground).
+                setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground)).setOngoing(true)
+                .build();
+    }
+
+
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is not in the Support Library.
@@ -23,13 +59,13 @@ public class MainActivity extends AppCompatActivity {
             CharSequence name = getString(R.string.notification_channel_name);
             String description = getString(R.string.notification_channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
+            notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            notificationChannel.setDescription(description);
             // Register the channel with the system. You can't change the importance
             // or other notification behaviors after this.
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }else{
+            notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        } else {
             ;
         }
     }
